576bb988e9476fb6a09cdd48b49770bb94c5af35
Henry Southall <hen0104.hs@gmail.com>
2019-10-05 17:42:21 +0100

    Incorporating MSAA resolve into its own method.

--- D3DRenderer1/Shaders/DeferredQuad/pixelShader.hlsl
+++ D3DRenderer1/Shaders/DeferredQuad/pixelShader.hlsl
@@ -8,6 +8,15 @@ struct VS_OUT
 	float2 texcoord : TEXCOORD0;
 };
 
+float4 MSAAResolve(Texture2DMS<float4> inputTexture, int numSamples, uint2 pixelTexCoords)
+{
+	float4 result = 0.0f;
+	for (int i = 0; i < numSamples; i++)
+		result += inputTexture.sample[i][pixelTexCoords];	//Combine all the samples
+	result /= numSamples;	//And calculate average. MSAA!
+	return result;
+}
+
 float4 main(VS_OUT input) : SV_TARGET0
 {
 
@@ -15,10 +24,7 @@ float4 main(VS_OUT input) : SV_TARGET0
 	resultTex.GetDimensions(newTexCoords.x, newTexCoords.y, newTexCoords.z);
 	newTexCoords.x = input.texcoord.x * newTexCoords.x;
 	newTexCoords.y = input.texcoord.y * newTexCoords.y;
-
-	float4 albedo = 0.0f;
-	for (int i = 0; i < newTexCoords.z; i++)
-		albedo += resultTex.sample[i][newTexCoords.xy];	//Combine all the samples
-	albedo /= newTexCoords.z;	//And calculate average. MSAA!
+	
+	float4 albedo = MSAAResolve(resultTex, newTexCoords.z, newTexCoords.xy);
 	return albedo;
 }
