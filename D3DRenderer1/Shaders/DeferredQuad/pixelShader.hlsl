e8117f3d9d4e2ebb823f1a21a1256c18d85353c8
Henry Southall <hen0104.hs@gmail.com>
2019-10-05 16:47:12 +0100

    Add some basic passthrough quad shaders for the deferred pipeline. soon(tm)

---
+++ D3DRenderer1/Shaders/DeferredQuad/pixelShader.hlsl
@@ -0,0 +1,4 @@
+float4 main() : SV_TARGET0
+{
+	return float4(1,0,0,1);
+}
---
+++ D3DRenderer1/Shaders/DeferredQuad/vertexShader.hlsl
@@ -0,0 +1,18 @@
+struct VS_INPUT
+{
+	float3 position : POSITION;
+	float3 normal : NORMAL;
+	float3 texcoord : TEXCOORD;
+};
+
+struct VS_OUT
+{
+	float4 position : SV_POSITION;
+};
+
+VS_OUT main(VS_INPUT input)
+{
+	VS_OUT output;
+	output.position = float4(input.position, 1.0f);
+	return output;
+}
